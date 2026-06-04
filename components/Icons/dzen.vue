6d47f5aa9fa2290604b2e81a2df59ea6375bba78
supsik <hasboliev@gmail.com>
2026-06-04 11:27:29 +0300

    footer desktop fix

---
+++ components/Icons/dzen.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="42"
+    height="42"
+    viewBox="0 0 42 42"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_93_70)">
+      <rect width="42" height="42" rx="8" fill="currentColor" />
+      <path
+        d="M42 21.225V20.775C32.7 20.475 28.455 20.25 25.05 16.95C21.75 13.545 21.51 9.3 21.225 0H20.775C20.475 9.3 20.25 13.545 16.95 16.95C13.545 20.25 9.3 20.49 0 20.775V21.225C9.3 21.525 13.545 21.75 16.95 25.05C20.25 28.455 20.49 32.7 20.775 42H21.225C21.525 32.7 21.75 28.455 25.05 25.05C28.455 21.75 32.7 21.51 42 21.225Z"
+        fill="#0C1030"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_93_70">
+        <rect width="42" height="42" rx="8" fill="currentColor" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,56 +1,151 @@
 <template>
 	<div class="footer container">
 		<nuxt-link class="footer__logo" to="/">GoodTech</nuxt-link>
-		<nav class="footer__navigation">
-			<ul>
-				<li v-for="item in layoutNavigation">
-					<nuxt-link :to="item.link">{{ item.name }}</nuxt-link>
-				</li>
-			</ul>
-		</nav>
-		<a href="#request-form" class="order__btn">Оставить заявку</a>
+		<div class="footer__content">
+			<div class="footer__description">
+				<p>
+					GoodTech - Nulla consectetur esse fugiat deserunt anim adipiscing
+					pariatur veniam laborum ut fugiat. Est deserunt cillum id consectetur
+					dolor minim velit.
+				</p>
+				<a href="#request-form" class="order__btn">Оставить заявку</a>
+			</div>
+			<nav class="footer__list">
+				<h3>Навигация</h3>
+				<ul>
+					<li v-for="item in layoutNavigation">
+						<nuxt-link :to="item.link">{{ item.name }}</nuxt-link>
+					</li>
+				</ul>
+			</nav>
+			<nav class="footer__list">
+				<h3>Контакты</h3>
+				<ul>
+					<li><a href="tel:+79999999999">+7 (999) 999-99-99</a></li>
+					<li><a href="tel:+79999999999">+7 (999) 999-99-99</a></li>
+					<li><a href="mailto:info@goodtech.ru">info@goodtech.ru</a></li>
+				</ul>
+			</nav>
+			<div class="footer__list soc-section">
+				<h3>Социальные сети</h3>
+				<ul>
+					<li>
+						<a href="#" target="_blank"><icons-vkontakte /></a>
+					</li>
+					<li>
+						<a href="#" target="_blank"><icons-telegram /></a>
+					</li>
+					<li>
+						<a href="#" target="_blank"><icons-dzen /></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="footer__bottom">
+			<span>© 2026 ООО «GoodTech»</span>
+			<div class="footer__list soc-section">
+				<ul>
+					<li>
+						<a href="#" target="_blank"><icons-vkontakte /></a>
+					</li>
+					<li>
+						<a href="#" target="_blank"><icons-telegram /></a>
+					</li>
+					<li>
+						<a href="#" target="_blank"><icons-dzen /></a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 <script setup lang="ts">
 interface Navigation {
-	name: String,
-	link: string,
+	name: String;
+	link: string;
 };
 
 const props = defineProps({
 	layoutNavigation: Array<Navigation>,
 });
 </script>
-<style lang='scss'>
+<style lang="scss">
 .footer {
 	background-color: #0c1030;
-	padding-block: 52px 32px;
+	padding-top: 52px;
 	margin-top: 52px;
 
 	.order__btn {
-		margin-top: 42px;
+		margin-top: 52px;
 		max-width: 240px;
 	}
 
 	@include mq($lg) { padding-block: 32px 24px }
 }
+.footer__content {
+	margin-top: 32px;
+	display: flex;
+	gap: 80px;
 
-.footer__navigation {
-	margin-top: 24px;
+	h3 {
+		font-size: 24px;
+		font-weight: 500;
+	}
+}
+
+.footer__description { max-width: 520px }
+
+.footer__list {
+	&:first-of-type { margin-left: auto }
+
+	&.soc-section {
+		ul {
+			flex-direction: row;
+			gap: 24px;
+		}
+	}
 
 	ul {
 		display: flex;
 		flex-direction: column;
 		gap: 18px;
+		margin-top: 20px;
 	}
 
 	a {
+		display: block;
 		color: #ffffff76;
 		position: relative;
-		transition: .2s;
+		transition: 0.2s;
 		font-size: 20px;
+		font-weight: 500;
 
-		&:hover { color: #FFF }
+		&:hover { color: #fff }
+	}
+}
+
+.footer__bottom {
+	display: flex;
+	align-items: center;
+	margin-top: 100px;
+	padding-block: 24px;
+	border-top: 1px solid rgba($color: #FFF, $alpha: .2);
+	font-weight: 500;
+
+	& * { color: rgba($color: #FFF, $alpha: .2) }
+	
+	.soc-section {
+		ul { margin-top: 0 }
+
+		li {
+			width: 24px;
+			height: 24px;
+
+			svg {
+				width: 100%;
+				height: 100%;
+			}
+		}
 	}
 }
 </style>
